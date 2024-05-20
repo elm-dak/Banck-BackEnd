067b5af419cf74bbcf2853904f2987cbd3d46ef4
El Mestapha DAKOUKY <mestafa.wydad66@gmail.com>
2024-05-20 19:54:54 +0100

    changes

--- src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
@@ -35,7 +35,7 @@ public class EbankingBackendApplication {
     @Bean
     CommandLineRunner commandLineRunner(BankAccountService bankAccountService){
         return args -> {
-           Stream.of("Hassan","Imane","Mohamed").forEach(name->{
+           Stream.of("Mustapha","Younesse","Yassine").forEach(name->{
                CustomerDTO customer=new CustomerDTO();
                customer.setName(name);
                customer.setEmail(name+"@gmail.com");
@@ -70,7 +70,7 @@ public class EbankingBackendApplication {
                             BankAccountRepository bankAccountRepository,
                             AccountOperationRepository accountOperationRepository){
         return args -> {
-            Stream.of("Hassan","Yassine","Aicha").forEach(name->{
+            Stream.of("Mohameed","zakraia","reda").forEach(name->{
                 Customer customer=new Customer();
                 customer.setName(name);
                 customer.setEmail(name+"@gmail.com");
